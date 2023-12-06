b4f8070844dea5b44cc3a6853b1352062105b5bf
mchlleliu <mchelle.liu@mail.utoronto.ca>
2023-12-06 16:42:43 -0500

    make dataframes for DESeq2

--- SSAV_DESeq.R
+++ SSAV_DESeq.R
@@ -11,7 +11,6 @@
 
 # set path to data files
 Data_path <- "~/Desktop/UofT/PRJ1/ReadCount_KG_renamed/"
-files <- list.files(Data_path)
 
 # Packages
 ##########
@@ -54,3 +53,46 @@ library(ggstatsplot)
 
 ##########
 
+
+# make dataframe for DESeq2
+##########
+
+# list the files by category
+# the ordering is based on treatment(A/C)_rep(1:6)_sex(F/M)
+files <- list.files(Data_path)
+
+# chop off the ".tsv" from those file names
+samplename <- gsub('.{4}$', '', files)
+
+# set up factor for condition "sex" (must match order that files are listed; see "samplename" and "files")
+sex <- factor(c(rep(c("Female", "Male"), each = 2, times = 6), # Af, Am
+                rep("Male", times = 12)))  # Cm
+
+# set up factor for each genotype (Red or NR) "geno" (must match order that files are listed; see "samplename" and "files")
+geno <- factor(c(rep(c("NR", "Red"), each = 1, times = 18)))
+
+# set up factor for each treatment (A or C) "trt" (must match order that files are listed; see "samplename" and "files")
+trt <- factor(c(rep("A", 24), 
+                rep("C", 12)))
+
+# set up factor for each treatment (Af, Am, or Cm) "trt2" 
+trt2 <- factor(c(rep(c("Af", "Am"), each = 2, times = 6), 
+                 rep("Cm", 12)))
+
+# set up factor for batch "rep" (must match order that files are listed; see "samplename" and "files")
+rep <- factor(c(as.character(c(rep(1:6, each = 4, times = 1), # 6 replicates for each of the 4 sex-geno in A
+                               rep(1:6, each = 2, times = 1)))))  # 6 replicates for each of the male-geno in C
+
+
+# put those into a dataframe 
+sampleTable <- data.frame(sampleName = samplename, 
+                          fileName = files, 
+                          sex = sex,
+                          geno = geno,
+                          trt = trt,
+                          trt2 = trt2,
+                          rep = rep)
+
+str(sampleTable) # Check that factors are factors, else use e.g.: sampleTable$rep <- factor(sampleTable$rep)
+
+##########
