fbadd6dc1598a5659ae4f8c0d002baa399a5390d
mchlleliu <mchelle.liu@mail.utoronto.ca>
2023-12-06 16:29:42 -0500

    add library packages

---
+++ SSAV_DESeq.R
@@ -0,0 +1,52 @@
+###################################
+#
+#                             Grieshop et al. 2023
+#             DsRed experimental evolution - transcriptomics analysis
+#             Data were prepared using file DsRed_transcriptomics.txt
+# 
+# 
+###################################
+
+# rm(list=ls()) # Clears the environment 
+
+
+# Packages
+##########
+
+# if needed
+# install.packages("BiocManager")
+# BiocManager::install("DESeq2")
+# BiocManager::install("apeglm")
+# BiocManager::install("ashr")
+# BiocManager::install("mltools")
+
+library(DESeq2)
+library(apeglm)
+library(ashr)
+library(vsn)
+library(hexbin)
+library(ggplot2)
+library(ggblend)
+library(ggpubr)
+library(gridExtra)
+library(plyr)
+library(dplyr)
+library(RColorBrewer)
+library(pheatmap)
+library(car)
+library(lmtest)
+library(lmPerm)
+library(Rmisc)
+library(Hmisc)
+library(mltools)
+library(boot)
+library(clipr) 
+library(broom)
+library(tidyr)
+library(extrafont)
+library(scales)
+library(readxl)
+library(cowplot)
+library(ggstatsplot)
+
+##########
