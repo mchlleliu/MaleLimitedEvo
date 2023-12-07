d567e51337965a87c06e4e618e2e4f6bf1a8b71b
mchlleliu <mchelle.liu@mail.utoronto.ca>
2023-12-07 17:09:39 -0500

    Clean up code, clear sections

--- SSAV_dimorphism.R
+++ SSAV_dimorphism.R
@@ -75,144 +75,30 @@ A.Red.m.NR.f.SBGE <- SBGE_log2FC
 ##########
 
 
-# Get Mishra et al.'s data 
-##########
-
-ASE <- read.delim(file="~/Desktop/UofT/PRJ1/data/DifferentialGeneExpression.whole.bodies.tsv", sep="\t", header=TRUE)
-# Grab the desired variables from there
-ASE <- data.frame(cbind(ASE$log2FoldChange,
-                        ASE$lfcSE,
-                        ASE$FlyBaseID))
-colnames(ASE) <- c("exp_SBGE_ase", "se_SBGE_ase", "FlyBaseID")
-# fix formatting
-ASE$exp_SBGE_ase <- as.numeric(ASE$exp_SBGE_ase)
-ASE$se_SBGE_ase <- as.numeric(ASE$se_SBGE_ase)
-str(ASE)
-
-
-# Define three levels of SBGE categorization 
-x1 = 1 # first cut-off (FBG < -1, MBG > 1 , -1 < UBG < 1)
-x2 = 5 # second cut-off (extreme FBG < -5, extreme MBG > 5)
-# y0 = 1 # tolerance of middle bins ### I DONT GET THIS 
-# # xmid1 = (x1 + x2)/2
-
-# Simple (3 levels)
-# one level of female-biased gene expression
-fbg.keep <- ASE$exp_SBGE_ase < -x1 & (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < 0
-fbg <- ASE[fbg.keep,]
-fbg$SBGE_simp <- rep(c("a.fbg"), dim(fbg)[1])
-# one unbiased category
-ubg.keep <- ASE$exp_SBGE_ase < x1 & ASE$exp_SBGE_ase > -x1 & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > -(x1+y0) & (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < (x1+y0)
-ubg <- ASE[ubg.keep,]
-ubg$SBGE_simp <- rep(c("b.ubg"), dim(ubg)[1])
-# two levels of male-biased gene expression
-mbg.keep <- ASE$exp_SBGE_ase > x1 & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > 0
-mbg <- ASE[mbg.keep,]
-mbg$SBGE_simp <- rep(c("c.mbg"), dim(mbg)[1])
-# 1 gene is tossed out b/c the uncertainty in its estimate breaches a cutoff boundary 
-ASE <- rbind(fbg, mbg, ubg)
-str(ASE)
-
-# Complex (5 levels)
-# two levels of female-biased gene expression (NOTE: I don't get this end term: -(x2-y0))
-more.fbg.keep <- ASE$exp_SBGE_ase < -x2 & (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < -(x2-y0) # extreme FBG < -5
-more.fbg <- ASE[more.fbg.keep,]
-more.fbg$SBGE_comp <- rep(c("a.more.fbg"), dim(more.fbg)[1])
-#
-fbg.keep <- ASE$exp_SBGE_ase < -x1 & ASE$exp_SBGE_ase > -x2 & (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < 0 & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > -(x2+y0)
-fbg <- ASE[fbg.keep,]
-fbg$SBGE_comp <- rep(c("b.fbg"), dim(fbg)[1])
-# one unbiased category
-ubg.keep <- ASE$exp_SBGE_ase < x1 & ASE$exp_SBGE_ase > -x1 & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > -(x1+y0) & (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < (x1+y0)
-ubg <- ASE[ubg.keep,]
-ubg$SBGE_comp <- rep(c("c.ubg"), dim(ubg)[1])
-# two levels of male-biased gene expression
-mbg.keep <- ASE$exp_SBGE_ase > x1 & ASE$exp_SBGE_ase < x2 & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > 0 & (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < (x2+y0)
-mbg <- ASE[mbg.keep,]
-mbg$SBGE_comp <- rep(c("d.mbg"), dim(mbg)[1])
-#
-more.mbg.keep <- ASE$exp_SBGE_ase > x2 & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > (x2-y0)
-more.mbg <- ASE[more.mbg.keep,]
-more.mbg$SBGE_comp <- rep(c("e.more.mbg"), dim(more.mbg)[1])
-# 3 genes tossed out  b/c the uncertainty in their estimate breaches a cutoff boundary
-ASE <- rbind(more.fbg, fbg, ubg, mbg, more.mbg)
-str(ASE)
-
-
-# Define three levels of SBGE categorization based on Singh and Agrawal 2023
-x1 = 0.5 # first cut-off 
-x2 = 2 # second cut-off 
-x3 = 5 # third cut-off
-y0 = 1 # tolerance of middle bins ### I DONT GET THIS 
-# xmid1 = (x1 + x2)/2
-
-# More Complex (7 levels)
-# three levels of female-biased gene expression
-ext.fbg.keep <- ASE$exp_SBGE_ase < -x3 & 
-  (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) < -(x3-y0) # extreme FBG < -5
-ext.fbg <- ASE[ext.fbg.keep,]
-ext.fbg$SBGE_singh <- rep(c("a.ext.fbg"), dim(ext.fbg)[1])
-#
-more.fbg.keep <- ASE$exp_SBGE_ase < -x2 & ASE$exp_SBGE_ase > -x3 & 
-  (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < -(x2+y0) &
-  (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > -(x3+y0) 
-more.fbg <- ASE[more.fbg.keep,]
-more.fbg$SBGE_singh <- rep(c("b.more.fbg"), dim(more.fbg)[1])
-#
-fbg.keep <- ASE$exp_SBGE_ase < -x1 & ASE$exp_SBGE_ase > -x2 & 
-  (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < 0 &
-  (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > -(x2+y0)
-fbg <- ASE[fbg.keep,]
-fbg$SBGE_singh <- rep(c("c.fbg"), dim(fbg)[1])
-# one unbiased category
-ubg.keep <- ASE$exp_SBGE_ase < x1 & ASE$exp_SBGE_ase > -x1 & 
-  (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > -(x1+y0) & (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < (x1+y0)
-ubg <- ASE[ubg.keep,]
-ubg$SBGE_singh <- rep(c("d.ubg"), dim(ubg)[1])
-# three levels of male-biased gene expression
-mbg.keep <- ASE$exp_SBGE_ase > x1 & ASE$exp_SBGE_ase < x2 & 
-  (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > 0 & 
-  (ASE$exp_SBGE_ase + ASE$se_SBGE_ase) < (x2+y0)
-mbg <- ASE[mbg.keep,]
-mbg$SBGE_singh <- rep(c("e.mbg"), dim(mbg)[1])
-#
-more.mbg.keep <- ASE$exp_SBGE_ase > x2 & ASE$exp_SBGE_ase < x3 &
-  (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > (x2-y0) & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) < (x3-y0)
-more.mbg <- ASE[more.mbg.keep,]
-more.mbg$SBGE_singh <- rep(c("f.more.mbg"), dim(more.mbg)[1])
-#
-ext.mbg.keep <- ASE$exp_SBGE_ase > x3 & (ASE$exp_SBGE_ase - ASE$se_SBGE_ase) > (x3-y0)
-ext.mbg <- ASE[ext.mbg.keep,]
-ext.mbg$SBGE_singh <- rep(c("g.ext.mbg"), dim(ext.mbg)[1])
-# 3 genes tossed out  b/c the uncertainty in their estimate breaches a cutoff boundary
-ASE <- rbind(ext.fbg, more.fbg, fbg, ubg, mbg, more.mbg, ext.mbg)
-str(ASE)
-
-##########
-
-
 # Compare with ASE population
 ##########
-# Load Prashastha's ASE data
-ASE <- 
-
 # Merge Natural and Unnatural SBGE estimates.
 Sex_Bias_Nat_UnNat <- merge(A.Red.m.NR.f.SBGE, A.NR.m.Red.f.SBGE, by = "FlyBaseID", all = T)
-colnames(Sex_Bias_Nat_UnNat) <- c("FlyBaseID", "log2FC_Nat", "lfcSE_Nat", "p_Nat", "SBGE_simp.Nat", "SBGE_comp.Nat",
+# rename columns to something more sensible
+colnames(Sex_Bias_Nat_UnNat) <- c("FlyBaseID",
+                                  "log2FC_Nat", "lfcSE_Nat", "p_Nat", "SBGE_simp.Nat", "SBGE_comp.Nat",
                                   "log2FC_UnNat", "lfcSE_UnNat", "p_UnNat", "SBGE_simp.UnNat", "SBGE_comp.UnNat")
+
+# Find difference between Natural and Unnatural log2FC estimates
 Sex_Bias_Nat_UnNat$diff <- Sex_Bias_Nat_UnNat$log2FC_Nat - Sex_Bias_Nat_UnNat$log2FC_UnNat
+
+# merge with outsourced log2FC estimates
 Sex_Bias_Nat_UnNat <- merge(Sex_Bias_Nat_UnNat, ASE, by = "FlyBaseID", all = T)
+Sex_Bias_Nat_UnNat <- merge(Sex_Bias_Nat_UnNat, SDIU, by = "FlyBaseID", all = T)
 
 
 # compare with ASE population
-Sex_Bias_Nat_UnNat$diff_Nat.ASE <- abs(Sex_Bias_Nat_UnNat$log2FC_Nat) - abs(Sex_Bias_Nat_UnNat$exp_SBGE_ase)
-Sex_Bias_Nat_UnNat$diff_UnNat.ASE <- abs(Sex_Bias_Nat_UnNat$log2FC_UnNat) - abs(Sex_Bias_Nat_UnNat$exp_SBGE_ase)
+Sex_Bias_Nat_UnNat$diff_Nat.ASE <- Sex_Bias_Nat_UnNat$log2FC_Nat - Sex_Bias_Nat_UnNat$exp_SBGE_ase
+Sex_Bias_Nat_UnNat$diff_UnNat.ASE <- Sex_Bias_Nat_UnNat$log2FC_UnNat - Sex_Bias_Nat_UnNat$exp_SBGE_ase
 
 # compare with OSADA population
-Sex_Bias_Nat_UnNat <- merge(Sex_Bias_Nat_UnNat, SDIU, by = "FlyBaseID", all = T)
-Sex_Bias_Nat_UnNat <- subset(Sex_Bias_Nat_UnNat, select=c(1:18,23:25))
-Sex_Bias_Nat_UnNat$diff_Nat.OSADA <- abs(Sex_Bias_Nat_UnNat$log2FC_Nat) - abs(Sex_Bias_Nat_UnNat$Whole.SBGE.Osada)
-Sex_Bias_Nat_UnNat$diff_UnNat.OSADA <- abs(Sex_Bias_Nat_UnNat$log2FC_UnNat) - abs(Sex_Bias_Nat_UnNat$Whole.SBGE.Osada)
+Sex_Bias_Nat_UnNat$diff_Nat.OSADA <- Sex_Bias_Nat_UnNat$log2FC_Nat - Sex_Bias_Nat_UnNat$Whole.SBGE.Osada
+Sex_Bias_Nat_UnNat$diff_UnNat.OSADA <- Sex_Bias_Nat_UnNat$log2FC_UnNat - Sex_Bias_Nat_UnNat$Whole.SBGE.Osada
 
 
 # UnNat_ASE, Nat_ASE, Nat_UnNat, Nat_OSADA, UnNat_OSADA
@@ -228,3 +114,10 @@ Nat_UnNat <- ggplot(Sex_Bias_Nat_UnNat[!is.na(Sex_Bias_Nat_UnNat$log2FC_Nat) & !
   geom_vline(xintercept = 0, size = 0.5, linetype= "dashed", color = "black", alpha = 0.7)
 
 ##########
+
+
+## conclusions: 
+# degree of dimorphism in OSADA population (DGRP flies x Mel6 Benin, West Africa) more similar to SSAV.
+# ASE population more dimorphic compared to SSAV and OSADA populations.
+# Natural pair less correlation to dimorphism in ASE and OSADA populations compared to UnNatural pair
+
