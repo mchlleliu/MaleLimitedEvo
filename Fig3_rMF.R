85c79706fa4a8a5279135aea9c02427b5df6b097
mchlleliu <mchelle.liu@mail.utoronto.ca>
2024-07-05 14:00:21 -0400

    add figure plotting functions

--- Fig3_rMF.R
+++ Fig3_rMF.R
@@ -130,28 +130,31 @@ pointSEplot <- function(boot_dat, perm_dat, x_col, SBGE_cat = NA){
     perm_dat$pval <- as.numeric(perm_dat$pval)
     y_count_FALSE <- boot_dat[!boot_dat$Sig,] %>% 
       dplyr::group_by(.[[SBGE_cat]]) %>%
-      dplyr::summarise(max = q95 + 0.05) %>% # 0.05
+      dplyr::summarise(y_count_FALSE = q95 + 0.05) %>% # 0.05
       dplyr::rename({{SBGE_cat}} := 1)
+    
     y_count_TRUE <- boot_dat[boot_dat$Sig,] %>%
       dplyr::group_by(.[[SBGE_cat]]) %>%
-      dplyr::summarise(max = q95 + 0.05) %>% # 0.05
+      dplyr::summarise(y_count_TRUE = q95 + 0.05) %>% # 0.05
       dplyr::rename({{SBGE_cat}} := 1)
   }
   else{
     y_count_FALSE <- boot_dat[!boot_dat$Sig,] %>% 
-      dplyr::summarise(max = q95 + 0.05)
+      dplyr::summarise(y_count_FALSE = q95 + 0.05)
     y_count_TRUE <- boot_dat[boot_dat$Sig,] %>% 
-      dplyr::summarise(max = q95 + 0.05)
+      dplyr::summarise(y_count_TRUE = q95 + 0.05)
     perm_dat <- as.data.frame(t(perm_dat))
     colnames(perm_dat) <- c("obs_diff", "n_TRUE", "n_FALSE", "pval", "Sig")
   }
-  # join y-axis coordinate with text dataframes
-  perm_dat$y_count_FALSE <- y_count_FALSE$max
-  perm_dat$y_count_TRUE <- y_count_TRUE$max
+  # join y-axis coordinate with text data frames
+  perm_dat <- merge(perm_dat, y_count_FALSE, by = "exp")
+  perm_dat <- merge(perm_dat, y_count_TRUE, by = "exp")
+  
   perm_dat$y_star <- max(perm_dat$y_count_FALSE, perm_dat$y_count_TRUE) + 0.1
   perm_dat$y_star <- ifelse(perm_dat$y_star > 0.95, 0.85, perm_dat$y_star)
   str(perm_dat)
   
+  # plot with grouping
   if(!is.na(SBGE_cat)){
     pointSE_plot <- ggplot(boot_dat) + 
       geom_point(aes_string(SBGE_cat, "q50", color = "Sig"), 
@@ -173,6 +176,8 @@ pointSEplot <- function(boot_dat, perm_dat, x_col, SBGE_cat = NA){
       scale_x_discrete(labels = c("Highly FB","Female-Biased", "Unbiased", "Male-Biased", "Highly MB")) +
       geom_vline(xintercept = c(seq(1.5, length(perm_dat[[SBGE_cat]]) )), color = "grey")
   }
+  
+  # regular plot (without grouping)
   else{
     pointSE_plot <- ggplot(boot_dat) +
       geom_point(aes(x = Sig, y = q50, color = Sig), show.legend = FALSE,
@@ -193,6 +198,8 @@ pointSEplot <- function(boot_dat, perm_dat, x_col, SBGE_cat = NA){
       labs(x = "\nSex-biased Gene Expression", y = print(x_col)) 
   }
   
+  
+  # general theme settings
   pointSE_plot <- pointSE_plot +
     scale_colour_manual(values = c("#AA4499", "#009E73"), # "red3", "steelblue3", "#888888" # "purple3", "chartreuse3", "orange2", "#E69F00", "#009E73"
                         labels = c("Background", "DE")) + # "Chr-2", "Chr-3", "X-Chr"
@@ -204,7 +211,6 @@ pointSEplot <- function(boot_dat, perm_dat, x_col, SBGE_cat = NA){
     theme(plot.title.position = c("panel"),
           legend.title = element_blank(),
           legend.position = c("bottom"),
-          # legend.box.background = element_rect(),
           legend.text = element_text(size=30, margin = margin(10,0,10,0), color = "black", vjust = -0.2),
           axis.text.x = element_text(size=30, margin = margin(5,0,0,0), color = "black"),
           axis.text.y = element_text(size=20, margin = margin(0,5,0,0), color = "black"),
@@ -250,73 +256,6 @@ boot_rMF_A.f <- TwoBoot(SSAV.geno_rMF[!is.na(SSAV.geno_rMF$A.f.Sig),], "rMF", "A
 ########
 
 
-
-# ------------- rMF by exp. magnitude ----------------
-# expression magnitude was calculated in Singh & Agrawal 2023, using data from SEBIDA
-SDIU <- read.csv(file="~/Desktop/UofT/SSAV_RNA/Data/SBGEandSSSdataForMBE.csv", sep=",", header=TRUE)
-colnames(SDIU)[2] <- "FlyBaseID"
-
-
-SSAV.geno_rMF <- merge(SSAV.geno_rMF, SDIU[,c("FlyBaseID", "log.avgExp.AdultLarva")], by = "FlyBaseID")
-SSAV.geno_rMF <- SSAV.geno_rMF[!is.na(SSAV.geno_rMF$log.avgExp.AdultLarva) &
-                                 !is.na(SSAV.geno_rMF$Sig) & 
-                                 !is.na(SSAV.geno_rMF$rMF),]
-
-rm(SDIU) # remove this data set from env
-
-# linear model testing whether SA candidate status is still a significant predictor of rMF,
-# controlling for rMF variation caused by expression level
-summary(lm(rMF ~ Sig + log.avgExp.AdultLarva + SBGE_comp, data = SSAV.geno_rMF))
-
-q <- quantile(SSAV.geno_rMF$log.avgExp.AdultLarva, na.rm = T, probs = c(0, 1/3, 2/3, 1))
-SSAV.geno_rMF_LOW <- SSAV.geno_rMF[SSAV.geno_rMF$log.avgExp.AdultLarva <= q[2],]
-SSAV.geno_rMF_MED <- SSAV.geno_rMF[SSAV.geno_rMF$log.avgExp.AdultLarva > q[2] &
-                                     SSAV.geno_rMF$log.avgExp.AdultLarva <= q[3],]
-SSAV.geno_rMF_HI <- SSAV.geno_rMF[SSAV.geno_rMF$log.avgExp.AdultLarva > q[3],]
-dim(SSAV.geno_rMF_HI)
-
-# by expression magnitude
-# low (0%-33%)
-perm_rMF_LOW <- TwoPerm(SSAV.geno_rMF_LOW, x_col = "rMF", groupBy = "Sig")
-boot_rMF_LOW <- TwoBoot(SSAV.geno_rMF_LOW, x_col = "rMF", groupBy = "Sig")
-rMF_all_LOW <- pointSEplot(boot_dat = boot_rMF_LOW, perm_dat = perm_rMF_LOW, x_col = "rMF") + coord_cartesian(ylim = c(0,1))
-# medium (33%-66%)
-perm_rMF_MED <- TwoPerm(SSAV.geno_rMF_MED, x_col = "rMF", groupBy = "Sig")
-boot_rMF_MED <- TwoBoot(SSAV.geno_rMF_MED, x_col = "rMF", groupBy = "Sig")
-rMF_all_MED <- pointSEplot(boot_dat = boot_rMF_MED, perm_dat = perm_rMF_MED, x_col = "rMF") + coord_cartesian(ylim = c(0,1))
-# high (66%-100%)
-perm_rMF_HI <- TwoPerm(SSAV.geno_rMF_HI, x_col = "rMF", groupBy = "Sig")
-boot_rMF_HI <- TwoBoot(SSAV.geno_rMF_HI, x_col = "rMF", groupBy = "Sig")
-rMF_all_HI <- pointSEplot(boot_dat = boot_rMF_HI, perm_dat = perm_rMF_HI, x_col = "rMF") + coord_cartesian(ylim = c(0,1))
-
-# combine low, medium, high expression
-exp_levels_rMF <- ggarrange(rMF_all + theme(axis.title.x = element_blank(),
-                                            axis.title.y = element_blank(), axis.text.x = element_blank()),
-                            ggarrange(rMF_all_LOW + theme(axis.title.x = element_blank(),
-                                                          axis.title.y = element_blank(),
-                                                          axis.text.x = element_blank()), 
-                                      rMF_all_MED + theme(axis.title.x = element_blank(),
-                                                          axis.title.y = element_blank(),
-                                                          axis.text.x = element_blank()), 
-                                      rMF_all_HI + theme(axis.title.x = element_blank(),
-                                                         axis.title.y = element_blank(),
-                                                         axis.text.x = element_blank()), 
-                                      ncol = 3),
-                            nrow = 2, heights = c(2, 1))
-
-
-# save plot
-# pdf(file = "~/Desktop/UofT/SSAV_RNA/Plots/test_rMF_exp.pdf",   # The directory you want to save the file in
-#     width = 14, # 12, 24, 20 The width of the plot in inches
-#     height = 14) # 10, 20, 13 The height of the plot in inches
-# 
-annotate_figure(exp_levels_rMF + theme(plot.margin = margin(10,10,10,0)),
-                left = text_grob("rMF",
-                                 rot = 90, size = 40))
-# dev.off()
-########
-
-
 ## ------------- rMF by SBGE categories ----------------
 ## for all candidates (combined male and female candidates)
 # run permutation
@@ -359,7 +298,6 @@ rMF_SBGE_A.m <- pointSEplot(boot_dat = boot_rMF_SBGE_A.m,
 #########
 
 
-
 ## ------------- plot All and SBGE together -----------------
 # All genes
 ###### 
@@ -390,6 +328,8 @@ Fig3_main <- pointSEplot(boot_dat = boot_All_SBGE_rMF,
 #######
 
 
+# Considering DE genes from male and female comparisons separately (Fig. S5):
+
 # Male candidates
 ######
 perm_All_SBGE_rMF_A.m <- c("a.all", perm_rMF_A.m)
@@ -409,7 +349,6 @@ Fig3A_suppl <- pointSEplot(boot_dat = boot_All_SBGE_rMF_A.m,
   geom_vline(xintercept = 1.5, color = "black", size = 1.5) +
   ylab(expression(italic("r"["MF"])))
 #######
-
 
 # Female candidates
 ######
@@ -434,16 +373,95 @@ Fig3B_suppl <- pointSEplot(boot_dat = boot_All_SBGE_rMF_A.f,
 
 
 
-# Figure 3
-pdf(file = "~/Desktop/UofT/SSAV_RNA/Plots/final_2/Fig3_main.pdf",   # The directory you want to save the file in
-    width = 17, # The width of the plot in inches
-    height = 9) # 9 18 The height of the plot in inches
+# Save figures
+# pdf(file = "~/Desktop/UofT/SSAV_RNA/Plots/final_2/Fig3_main.pdf",   # The directory you want to save the file in
+#     width = 17, # The width of the plot in inches
+#     height = 9) # 9 18 The height of the plot in inches
+
+# Suppl. Figure S5
 # ggarrange(Fig3A_suppl + theme(axis.text.x = element_blank()) + labs(x= ""),
 #           Fig3B_suppl + theme(axis.title.x = element_blank(), axis.text.x = element_text(size = 25)),
 #           labels = c("A)", "B)"),
 #           nrow = 2, heights = c(1, 0.9),
 #           common.legend = TRUE, legend = "bottom",
 #           font.label = list(size = 30), hjust = -0.01)
+
+# Fig. 3
 Fig3_main + theme(axis.title.x = element_blank(), legend.text = element_text(size = 30, vjust = 1),
                   axis.text.x = element_text(size = 25))
-dev.off()
+
+
+# dev.off()
+
+
+
+# (Figure S6)
+# ------------- rMF by exp. magnitude ----------------
+# expression magnitude was calculated in Singh & Agrawal 2023, using data from SEBIDA
+SDIU <- read.csv(file="~/Desktop/UofT/SSAV_RNA/Data/SBGEandSSSdataForMBE.csv", sep=",", header=TRUE)
+colnames(SDIU)[2] <- "FlyBaseID"
+
+# save this in another object for now...
+SSAV.geno_rMF_exp <- merge(SSAV.geno_rMF, SDIU[,c("FlyBaseID", "log.avgExp.AdultLarva")], by = "FlyBaseID")
+SSAV.geno_rMF_exp <- SSAV.geno_rMF_exp[!is.na(SSAV.geno_rMF_exp$log.avgExp.AdultLarva) &
+                                         !is.na(SSAV.geno_rMF_exp$Sig) & 
+                                         !is.na(SSAV.geno_rMF_exp$rMF),]
+
+rm(SDIU) # remove this data set from env
+
+# 1.
+# Linear model testing whether SA candidate status is still a significant predictor of rMF,
+# controlling for rMF variation caused by expression level
+summary(lm(rMF ~ Sig + log.avgExp.AdultLarva + SBGE_comp, data = SSAV.geno_rMF_exp))
+
+
+# 2.
+# Another way to test for this is to split the analysis based on exp levels
+# define expression level category (Low/Medium/High)
+q <- quantile(SSAV.geno_rMF_exp$log.avgExp.AdultLarva, na.rm = T, probs = c(0, 1/3, 2/3, 1))
+SSAV.geno_rMF_exp <- SSAV.geno_rMF_exp %>%
+  mutate(exp = ifelse(log.avgExp.AdultLarva <= q[2], "b.Low",
+                      ifelse(log.avgExp.AdultLarva > q[2] & log.avgExp.AdultLarva <= q[3], "c.Medium", 
+                             "d.High")))
+
+
+# remember to call the bootstrapping and permutation functions in "boot_permute.R"
+# NOTE: I made these functions originally to stratify based on SBGE categories (hence, "SBGE_cat"), 
+#       but they can also be used for grouping other categories, as long as you specify the
+#       column name which contains the categorical levels
+# permute the two groups (DE/Non-DE)
+perm_rMF_exp <- TwoPerm_SBGE(SSAV.geno_rMF_exp, x_col = "rMF", groupBy = "Sig", SBGE_cat = "exp")
+# bootstrap 95% conf. interval
+boot_rMF_exp <- TwoBoot_SBGE(SSAV.geno_rMF_exp, x_col = "rMF", groupBy = "Sig", SBGE_cat = "exp")
+
+# combine with unstratified analysis result
+perm_All_exp_rMF <- c("a.all", perm_rMF)
+perm_All_exp_rMF <- rbind(perm_All_exp_rMF, perm_rMF_exp)
+
+boot_All_exp_rMF <- boot_rMF
+boot_All_exp_rMF$exp <- "a.all"
+boot_All_exp_rMF <- rbind(boot_All_exp_rMF, boot_rMF_exp)
+
+
+# plot Figure S6.
+Fig_S6 <- pointSEplot(boot_dat = boot_All_exp_rMF,
+                      perm_dat = perm_All_exp_rMF, 
+                      x_col = "rMF", SBGE_cat = "exp") +
+  coord_cartesian(ylim = c(0,1)) +
+  scale_x_discrete(labels = c("All", "Low", "Medium", "High")) +
+  geom_vline(xintercept = 1.5, color = "black", size = 1.5) +
+  ylab(expression(italic("r"["MF"]))) +
+  theme(axis.title.x = element_blank(), 
+        legend.text = element_text(size = 30, vjust = 1),
+        axis.text.x = element_text(size = 25))
+
+
+# save plot
+# pdf(file = "~/Desktop/UofT/SSAV_RNA/Plots/final_2/rMF_Fig_S6.pdf",   # The directory you want to save the file in
+#     width = 12, # 12, 24, 20 The width of the plot in inches
+#     height = 7) # 10, 20, 13 The height of the plot in inches
+
+Fig_S6
+
+# dev.off()
+########
---
+++ Fig6_effectFuns.R
@@ -0,0 +1,132 @@
+
+# Figure 6 plotting functions
+
+
+# the two functions below are used in the corr plot function below
+# quad_count: counts the number of point in each of the 8 quadrants
+# variables: dat = data.frame object containing the values to be plotted
+#            x = values for group 1, plotted on the x-axis
+#            y = values for group 2, plotted on the y-axis
+#            lim = for plotting code. specificy the x-lim and y-lim of the plot
+# return: a data frame with number of points, 
+#         proportion relative to total points, 
+#         and plotting coordinates
+quad_count <- function(dat, x, y, lim = 5){
+  
+  dat <- dat[dat[[x]] != dat[[y]],]
+  # 
+  count <- dat %>%
+    # Count how many with each combination of X and Y being positive
+    dplyr::count(right = .[[x]] > 0, # on the right side of plot?
+                 top = .[[y]] > 0, # on the top side of plot?
+                 # for each quadrant, divide into two: 
+                 # (this is a bit weird, but needed so the numbers can be plotted at the right coordinates)
+                 UP = !top & abs(.[[x]]) > abs(.[[y]]) | # quadrant III up-left or quadrant IV up-right
+                   top & abs(.[[x]]) < abs(.[[y]])) %>%  # quadrant I up-right or quadrant II up-left
+    dplyr::mutate(perc = n/sum(n)) %>% # calculate percentage of points relative to total number of points
+    
+    # this is another strange one for setting up the coordinates
+    dplyr::mutate(conc = right & top | (!right & !top), # quadrant I and quadrant III (the concordant changes)
+                  dir_UP = conc & UP, # concordant changes where x > y
+                  dir_DOWN = conc & !UP) %>% # concordant changes where x < y
+    
+    # TRUE = 1, FALSE = 0
+    # specificy coordinates for texts on plot
+    dplyr::mutate(!!x := lim/2*(2*(right - 0.5)+(UP - 0.5)+((conc-0.001)*0.5)-(dir_UP*1.5)+(dir_DOWN*0.5)), 
+                  !!y := lim/2*(2*(top - 0.5)+(UP - 0.5)))
+  
+  print(count)
+  return(count)
+}
+
+
+# colour_quadrant: add a column to specify colour for each point based on where they are located,
+#                 for points in the concordant quadrants (I and III)
+# variables: dat = data.frame object containing the values to be plotted
+#            x = values for group 1, plotted on the x-axis
+#            y = values for group 2, plotted on the y-axis
+#            colx = colour for x > y
+#            coly = colour for x < y
+#            colNonCon = colour for quadrants II and IV
+# return: a data frame with color specified for each data point
+colour_quadrant <-  function(dat, x, y, colx, coly, colNonCon){
+  col <- dat %>%
+    # logical columns to define where the point is locates
+    mutate(right = .[[x]] > 0, # on the right part of plot?
+           top = .[[y]] > 0, # on the top part of plot?
+           # for the concordant quadrants (I & III)... 
+           DOWN = !top & abs(.[[x]]) > abs(.[[y]]) | # quadrant III where x > y
+             top & abs(.[[x]]) > abs(.[[y]])) %>% # quadrant I where x > y
+    # add the colour
+    mutate(quadrant = ifelse(right & top | (!right & !top), 
+                             ifelse(DOWN, colx, coly), 
+                             colNonCon))
+  return(col)
+}
+
+
+
+# plot_corr: add a column to specify colour for each point based on where they are located,
+#                 for points in the concordant quadrants (I and III)
+# variables: dat = data.frame object containing the values to be plotted
+#            x = values for group 1, plotted on the x-axis
+#            y = values for group 2, plotted on the y-axis
+#            colx = colour for x > y
+#            coly = colour for x < y
+#            colNonCon = colour for quadrants II and IV
+#            xlab = label for x-axis
+#            ylab = label for y-axis
+#            lim = x and y axes limit
+#            title = of graph
+plot_corr <- function(dat, x, y, colx, coly, colNonCon, xlab, ylab, lim, title){
+  # count the percentages
+  
+  # comment in if plotting figure 6
+  dat[[x]] = abs(dat[[x]])
+  dat[[y]] = abs(dat[[y]])
+  
+  quad_n <- quad_count(dat, x, y, lim)
+  # manage the colour of points
+  quad_col <- colour_quadrant(dat, x, y, colx, coly, colNonCon)
+  # plot
+  corr <- ggplot(dat, aes_string(x = x, y = y)) +
+    geom_point(size = 2, shape = 16, alpha = 0.5, color = quad_col$quadrant) +  
+    
+    # add lines to separate quadrants
+    # geom_abline(intercept = 0, slope = 0,  size = 0.5, linetype="solid", color = "black") +
+    # geom_hline(yintercept = 0,  size = 0.5, linetype="solid", color = "black") +
+    # geom_vline(xintercept = 0,  size = 0.5, linetype="solid", color = "black") +
+    geom_abline(intercept = 0, slope = 1,  size = 0.5, linetype="dashed", color = "black") +
+    # geom_abline(intercept = 0, slope = -1,  size = 0.5, linetype="dashed", color = "black") +
+    
+    # add percentages
+    geom_text(aes(label = paste(round(perc*100,digits=0),"%",sep="")), data = quad_n, size = 10) +
+    coord_cartesian(xlim=c(-lim, lim), ylim = c(-lim,lim)) + # change to (0, lim) or (-lim, lim) depending on the figure
+    labs(x = print(xlab), 
+         y = print(ylab) ,
+         title = print(title)) +
+    guides(color = guide_legend(override.aes = list(shape = c(NA, NA), # c(16, 16)
+                                                    size = c(4, 4),
+                                                    alpha = 1))) +
+    
+    # some theme settings...
+    theme_classic() +
+    theme(legend.title = element_blank(),
+          legend.position = c("None"),
+          #legend.justification = c("right", "bottom"),
+          #legend.box.just = "left",
+          #legend.box.background = element_rect(),
+          legend.box.background = element_rect(),
+          #legend.box.margin = margin(4, 6, 6, 6),
+          legend.text = element_text(size = 20, color = "black"),
+          plot.tag = element_text(size = 20, color = "black"),
+          axis.text.x = element_text(size=20, margin = margin(5,0,0,0), color = "black"),
+          axis.text.y = element_text(size=20, margin = margin(0,5,0,0), color = "black"), 
+          axis.title.x = element_text(size=30, margin = margin(10,0,0,0), color = "black"),
+          axis.title.y = element_text(size=30, margin = margin(0,10,0,0), color = "black"),
+          plot.title = element_text(size=40, margin = margin(0,0,0,0), color = "black"),
+          plot.margin = margin(6,6,6,6)
+    )
+  
+  return(corr) # return plot
+}
