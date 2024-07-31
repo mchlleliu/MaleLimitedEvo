7f82270cdeab46a8dab51f96b06f0d819a1dd61f
mchlleliu <mchelle.liu@mail.utoronto.ca>
2024-07-31 15:07:52 -0400

    source Chromosome file

--- DS_TableS1.S2.R
+++ DS_TableS1.S2.R
@@ -26,45 +26,9 @@ library(DESeq2)
 #########
 
 # Get chromosome locations  
-##########
+source("Chromosome_df.R")
 
-all.genes <- read.delim(file="~/Desktop/UofT/SSAV_RNA/Data/all.genes.tsv", sep="\t", header=FALSE)
-colnames(all.genes) = c("FlyBaseID")
-
-Xchr <- read.delim(file="~/Desktop/UofT/SSAV_RNA/Data/X.chromosome.genes.tsv", sep="\t", header=TRUE)
-colnames(Xchr) = c("FlyBaseID")
-Xchr$Chr <- rep("X", dim(Xchr)[1])
-
-Ychr <- read.delim(file="~/Desktop/UofT/SSAV_RNA/Data/Y.chromosome.genes.tsv", sep="\t", header=TRUE)
-colnames(Ychr) = c("FlyBaseID")
-Ychr$Chr <- rep("Y", dim(Ychr)[1])
-
-
-chr2L <- read.delim(file="~/Desktop/UofT/SSAV_RNA/Data/2L.chromosome.genes.tsv", sep="\t", header=FALSE)
-colnames(chr2L) = c("FlyBaseID")
-chr2L$Chr <- rep("2L", dim(chr2L)[1])
-chr2R <- read.delim(file="~/Desktop/UofT/SSAV_RNA/Data/2R.chromosome.genes.tsv", sep="\t", header=FALSE)
-colnames(chr2R) = c("FlyBaseID")
-chr2R$Chr <- rep("2R", dim(chr2R)[1])
-# 
-chr2 <- rbind(chr2L, chr2R)
-chr2$Chr <- rep("2", dim(chr2)[1])
-
-chr3L <- read.delim(file="~/Desktop/UofT/SSAV_RNA/Data/3L.chromosome.genes.tsv", sep="\t", header=FALSE)
-colnames(chr3L) = c("FlyBaseID")
-chr3L$Chr <- rep("3L", dim(chr3L)[1])
-chr3R <- read.delim(file="~/Desktop/UofT/SSAV_RNA/Data/3R.chromosome.genes.tsv", sep="\t", header=FALSE)
-colnames(chr3R) = c("FlyBaseID")
-chr3R$Chr <- rep("3R", dim(chr3R)[1])
-#
-chr3 <- rbind(chr3L, chr3R)
-chr3$Chr <- rep("3", dim(chr3)[1])
-
-Chrs <- rbind(Xchr, Ychr, chr2, chr3) # Not all genes; just X, Y, 2, and 3.
-Chrs$Chr <- as.factor(Chrs$Chr)
-##########
-
-# set up external data from Mishra et al. 2022
+# set up external SSS data from Mishra et al. 2022
 ########
 jseq.Mishra = read.table("JunctionSeq/SDIU_ase/JSresults/SDIU_ASEallGenes.results.txt",
                       sep = "\t", header = TRUE)
@@ -149,6 +113,7 @@ jseq.C.m.geno <- jseq.C.m.geno[jseq.C.m.geno$Chr!= "Y" & jseq.C.m.geno$Chr!= "4"
 SSAV.sample.types <- data.frame(sampleType = c("A.m", "A.f", "C.m")) %>% 
   mutate(raw.JS.table = paste0("jseq.",sampleType,".geno"))
 #######
+
 
 # SSAV sample types data table:
 SSAV.sample.types # check the format
@@ -241,11 +206,6 @@ SSAV.geno <- merge(SSAV.geno, Chrs, by = "FlyBaseID")
 
 SSAV.sample.types$DEdata <- c("A.m.geno", "A.f.geno", "C.m.geno", "SSAV.geno")
 #######
-
-# load external Singh & Agrawal 2023 list of SSS genes (Osada, 2017 populations) ######
-Osada <- read.csv(file="~/Desktop/UofT/SSAV_RNA/Data/SBGEandSSSdataForMBE.csv", sep=",", header=TRUE)
-colnames(Osada)[2] <- "FlyBaseID"
-######
 
 
 # generate data for Table S1 and Table S2
