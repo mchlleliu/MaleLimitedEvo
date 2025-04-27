204f931bd83e2eae478ca39079ff5031dde65d57
mchlleliu <115041979+mchlleliu@users.noreply.github.com>
2025-04-26 21:25:45 -0400

    Edit year in script header

--- Analyses/DE_analysis/DE_SBGEdist_Fig2.S8.S9.R
+++ Analyses/DE_analysis/DE_SBGEdist_Fig2.S8.S9.R
@@ -1,6 +1,6 @@
 ###################################
 #
-#                             Grieshop et al. 2024
+#                             Grieshop et al. 2025
 #                             Author: Michelle Liu
 #             DsRed experimental evolution - transcriptomics analysis
 #                  Distribution of SA candidates by sex-bias
